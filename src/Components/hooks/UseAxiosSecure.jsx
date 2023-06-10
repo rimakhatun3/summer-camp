38520c744b03b36dabb9917cc5c1b6c93395b556
rimakhatun3 <rimamnt46@gmail.com>
2023-06-10 11:29:27 +0600

    added payment

--- src/Components/hooks/UseAxiosSecure.jsx
+++ src/Components/hooks/UseAxiosSecure.jsx
@@ -2,14 +2,14 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+const axiosSecure = axios.create({
+  baseURL: 'http://localhost:5000', 
+});
 const useAxiosSecure = () => {
     const {logOut} = useContext(AuthContext)
      const navigate = useNavigate(); 
    
-     const axiosSecure = axios.create({
-       baseURL: 'http://localhost:5000', 
-     });
+     
    
      useEffect(() => {
        axiosSecure.interceptors.request.use((config) => {
--- src/pages/Dashboard/CheckOutForm.jsx
+++ src/pages/Dashboard/CheckOutForm.jsx
@@ -1,11 +1,28 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import useAxiosSecure from '../../Components/hooks/UseAxiosSecure';
+import { AuthContext } from '../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const CheckOutForm = ({price}) => {
-    console.log(price)
+    const {user} = useContext(AuthContext)
+   const [axiosSecure] = useAxiosSecure()
     const stripe = useStripe();
     const elements = useElements();
 const [cardError,setCardError] = useState('')
+const [clientSecret,setClientSecret] = useState('')
+const [transitionId,setTransitionId] = useState('')
+
+useEffect(()=>{
+    axiosSecure.post('/create-payment-intent',{price})
+    .then(res=>{
+        console.log(res.data.clientSecret)
+        setClientSecret(res.data.clientSecret)
+    })
+},[price,axiosSecure])
+
+
+
     const handleSubmit = async (event) => {
         event.preventDefault();
     
@@ -32,6 +49,55 @@ const [cardError,setCardError] = useState('')
             console.log('[PaymentMethod]', paymentMethod);
           }
 
+          const {paymentIntent, error:confirmError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card: card,
+                billing_details: {
+                  name: user?.name || "anonious",
+                  email:user?.email || "invalid email"
+                },
+              },
+            },
+          );
+          if(confirmError){
+            setCardError(confirmError.message)
+            
+          }
+        
+
+
+          console.log('paymentInten',paymentIntent)
+//   setProcessing(false)
+  if(paymentIntent.status=="succeeded"){
+setTransitionId(paymentIntent.id)
+  }
+
+  const payment ={
+    transationId :paymentIntent.id,
+    email:user?.email,
+    price,
+    date:new Date(),
+  
+  }
+
+  axiosSecure.post('/payment',payment)
+.then(res=>{
+  console.log(res.data)
+  
+
+  if(res.data.insertedId){
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'class added success',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+})
+
     }
     
     return (
@@ -53,11 +119,14 @@ const [cardError,setCardError] = useState('')
           },
         }}
       />
-      <button className='btn btn-sm bg-pink-600 mt-4' type="submit" disabled={!stripe}>
+      <button className='btn btn-sm bg-pink-600 mt-4' type="submit" disabled={!stripe  }>
         Pay
       </button>
     </form>
     {cardError && <p className='text-red-500 ml-5'>{cardError}</p>}
+    {
+  transitionId && <p className='text-green-800 m-3'>Transaction complete with transactionId: {transitionId}</p>
+}
         </div>
     );
 };
